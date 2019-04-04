472e9d9ead3d16bb97528e5f6ca8a2e8e0763dc1
nonybright <nonybrighto@gmail.com>
2019-04-04 07:21:49 +0100

    Implement social authentication

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -12,90 +12,124 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/constants.dart';
 
 class AuthService {
-
-  static const userUrl = kAppApiUrl+'/users/';
-  static const authUrl = kAppApiUrl+'/auth/';
+  static const userUrl = kAppApiUrl + '/users/';
+  static const authUrl = kAppApiUrl + '/auth/';
 
   AuthService();
 
   Future<User> authenticateWithFaceBook() async {
     var facebookLogin = new FacebookLogin();
     var result = await facebookLogin.logInWithReadPermissions(['email']);
+
     switch (result.status) {
       case FacebookLoginStatus.loggedIn:
-       try{
-
-          }catch(error){
-              throw Exception(error.message);
-          }
+        print(result.accessToken.token);
+        return _authenticateFacebookUserOnServer(result.accessToken.token);
         break;
       case FacebookLoginStatus.cancelledByUser:
-            throw Exception('Login Cancelled!');
+        throw Exception('Facebook Login Cancelled!');
         break;
       case FacebookLoginStatus.error:
-            throw Exception('Login Failed!');
+        throw Exception('Facebook Login Failed!');
         break;
+      default:
+        throw Exception('Facebook Login Failed!');
     }
   }
 
   Future<User> authenticateWithGoogle() async {
-
- try {
+    GoogleSignInAuthentication googleAuth;
+    try {
       GoogleSignIn _googleSignIn = GoogleSignIn();
       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-       GoogleSignInAuthentication googleAuth =   await googleUser.authentication;
-   
+      googleAuth = await googleUser.authentication;
     } catch (error) {
-        throw Exception(error.message);
+      throw Exception('Failed to connect to Google!');
     }
+    return _authenticateGoogleUserOnServer(googleAuth.idToken);
+  }
 
-}
-
-Future<User> signUpWithEmailAndPassword(username, email, password) async {
-  Response response;
-  try{
-     Dio dio = new Dio(); 
-      response = await dio.post(userUrl, data: {'username': username, 'email': email, 'password': password});
+  Future<User> signUpWithEmailAndPassword(username, email, password) async {
+    Response response;
+    try {
+      Dio dio = new Dio();
+      response = await dio.post(userUrl,
+          data: {'username': username, 'email': email, 'password': password});
       User authenticatedUser = User.fromJson(response.data['user']);
       String userJwtToken = response.data['token'];
       _saveUserDetailsToPreference(authenticatedUser, userJwtToken);
       return authenticatedUser;
-  } on DioError catch(error){
-    if(error.response != null){
+    } on DioError catch (error) {
+      if (error.response != null) {
         throw Exception(error.response.data['message']);
-    }else{
-        throw Exception('Error connecting to server');
-    } 
+      } else {
+        throw Exception('Error connecting to server!');
+      }
+    }
   }
-}
 
-Future<User> signInWithEmailAndPassword(loginDetial, password) async {
- 
-  Response response;
-  try{
-    Dio dio = new Dio(); 
-    response = await dio.post(authUrl+'login', data: {'username': loginDetial, 'password': password});
-    User authenticatedUser = User.fromJson(response.data['user']);
-    String userJwtToken = response.data['token'];
-    _saveUserDetailsToPreference(authenticatedUser, userJwtToken);
-    return authenticatedUser;
-  }catch(error){
-    if(error.response != null){
+  Future<User> signInWithEmailAndPassword(loginDetial, password) async {
+    Response response;
+    try {
+      Dio dio = new Dio();
+      response = await dio.post(authUrl + 'login',
+          data: {'username': loginDetial, 'password': password});
+      User authenticatedUser = User.fromJson(response.data['user']);
+      String userJwtToken = response.data['token'];
+      _saveUserDetailsToPreference(authenticatedUser, userJwtToken);
+      return authenticatedUser;
+    } catch (error) {
+      if (error.response != null) {
         throw Exception(error.response.data['message']);
-    }else{
-        throw Exception('Error connecting to server');
-    } 
+      } else {
+        throw Exception('Error connecting to server!');
+      }
+    }
   }
-}
 
-_saveUserDetailsToPreference(User user, String userJwtToken) async{
+  Future<User> _authenticateFacebookUserOnServer(String accessToken) async {
+    Response response;
+    try {
+      Dio dio = new Dio();
+      response = await dio.post(authUrl + 'facebook/token',
+          data: {'access_token': accessToken});
+      User authenticatedUser = User.fromJson(response.data['user']);
+      String userJwtToken = response.data['token'];
+      _saveUserDetailsToPreference(authenticatedUser, userJwtToken);
+      return authenticatedUser;
+    } catch (error) {
+      if (error.response != null) {
+        throw Exception(error.response.data['message']);
+      } else {
+        throw Exception('Error connecting to server!');
+      }
+    }
+  }
 
-      SharedPreferences pref = await SharedPreferences.getInstance();
-      pref.setString(kUserJwtTokenPrefKey, userJwtToken);
-      pref.setString(kUserIdPrefKey, user.id);
-      pref.setString(kUsernamePrefKey, user.username);
-      pref.setString(kUsernamePrefKey, user.email);
+  Future<User> _authenticateGoogleUserOnServer(String idToken) async {
+    Response response;
+    try {
+      Dio dio = new Dio();
+      response =
+          await dio.post(authUrl + 'google/token', data: {'id_token': idToken});
+      User authenticatedUser = User.fromJson(response.data['user']);
+      String userJwtToken = response.data['token'];
+      _saveUserDetailsToPreference(authenticatedUser, userJwtToken);
+      return authenticatedUser;
+    } catch (error) {
+      if (error.response != null) {
+        throw Exception(error.response.data['message']);
+      } else {
+        throw Exception('Error connecting to server');
+      }
+    }
+  }
 
-}
-
+  _saveUserDetailsToPreference(User user, String userJwtToken) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(kUserJwtTokenPrefKey, userJwtToken);
+    pref.setString(kUserIdPrefKey, user.id);
+    pref.setString(kUsernamePrefKey, user.username);
+    pref.setString(kUsernamePrefKey, user.email);
+  }
 }
